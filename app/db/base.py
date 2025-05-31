51ef7cbeeafcf4e216f0b861b83127c2a656287d
shaantanu98 <shantanubombatkar2@gmail.com>
2025-05-31 21:06:03 +0530

    fix issue

--- app/db/base.py
+++ app/db/base.py
@@ -4,10 +4,3 @@ Base = declarative_base()
 
 # Import all models here so Alembic's autogenerate can see them
 from app.db.models.user import User
-
-
-
-
-
-
-from app.db.models.product import Product
